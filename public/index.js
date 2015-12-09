743c5ef511e0d6a23a27d02e164e20e9fb2ea590
Teisia Park <teisia@gmail.com>
2015-12-09 13:58:47 -0700

    Module 2.2: As a developer, when I submit a get request on the playlist page [ https://lit-fortress-6467.herokuapp.com/object ] , I receive data and I am able to get all of the data .

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ var getter = $.ajax({
 getter.done(function(response) {
   var albums = response["results"];
   for (var i = 0; i < albums.length; i++) {
-    var randd = Math.floor((Math.random() * 3));
-    console.log(albums[randd]["cover_art"]);
+    //var randd = Math.floor((Math.random() * 3));
+    console.log(albums[i]);
   }
 });
