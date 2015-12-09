1d067aec4579eda2350013a8cf56f7e84949c313
Teisia Park <teisia@gmail.com>
2015-12-09 13:53:19 -0700

    Module 2.1: As a developer, when I submit a get request on the home page [https://lit-fortress-6467.herokuapp.com/object ] , I receive data and I am able to choose 3 random album image URLs from the data received.

---
+++ public/index.js
@@ -0,0 +1,13 @@
+var getter = $.ajax({
+  url: "https://lit-fortress-6467.herokuapp.com/object",
+  method: "GET",
+  dataType: "json"
+})
+
+getter.done(function(response) {
+  var albums = response["results"];
+  for (var i = 0; i < albums.length; i++) {
+    var randd = Math.floor((Math.random() * 3));
+    console.log(albums[randd]["cover_art"]);
+  }
+});
