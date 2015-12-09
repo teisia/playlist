1e136ecfa99d6debeaa81ff911184b2d26ab28ee
Teisia Park <teisia@gmail.com>
2015-12-09 14:14:18 -0700

    Module 2.3: As a developer, in a sandbox environment, I am able to make a POST request to the [ https://lit-fortress-6467.herokuapp.com/post ] receive a response, and log data from the response.

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 var getter = $.ajax({
-  url: "https://lit-fortress-6467.herokuapp.com/object",
-  method: "GET",
-  dataType: "json"
+  url: "https://lit-fortress-6467.herokuapp.com/post",
+  type: "POST"
 })
 
 getter.done(function(response) {
-  var albums = response["results"];
-  for (var i = 0; i < albums.length; i++) {
+  //var albums = response["results"];
+  //for (var i = 0; i < albums.length; i++) {
     //var randd = Math.floor((Math.random() * 3));
-    console.log(albums[i]);
-  }
+    console.log(response);
+    console.log("you got a response!");
+  //}
 });
